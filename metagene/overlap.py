b5cc71a2149495cedac6ca342b19cea1c2633987
Chang Ye <yech1990@gmail.com>
2022-07-22 19:49:07 -0500

    fix bug on strand

--- metagene/overlap.py
+++ metagene/overlap.py
@@ -20,13 +20,16 @@ def parse_features(feature_file_name: str) -> tuple[pd.DataFrame, dict]:
         sep="\t",
         names=["Chromosome", "Start", "End", "Name", "Index", "Strand"],
         comment="#",
-    ).assign(n_L=lambda x: x["End"] - x["Start"])
+    ).assign(len_of_windown=lambda x: x["End"] - x["Start"])
     df["Type"] = df["Name"].str.split(":").str[-1]
-    df["n_T"] = df.groupby("Name")["n_L"].transform("sum")
-    df["D"] = (df.groupby("Name")["n_L"].transform("cumsum") - df["n_L"]) / df[
-        "n_T"
-    ]
-    s = df.groupby("Type")["n_L"].sum()
+    df["len_of_feature"] = df.groupby("Name")["len_of_windown"].transform(
+        "sum"
+    )
+    df["frac_of_feature"] = (
+        df.groupby("Name")["len_of_windown"].transform("cumsum")
+        - df["len_of_windown"]
+    ) / df["len_of_feature"]
+    s = df.groupby("Type")["len_of_windown"].sum()
     return df, (s / s.sum()).to_dict()
 
 
@@ -59,7 +62,13 @@ def annotate_with_feature(
         )
         .apply(lambda x: x.nlargest(1, "Overlap"))
         .assign(
-            d=lambda x: ((x.Start + x.End) // 2 - x.Start_ref) / x.n_T + x.D
+            d=lambda x: np.where(
+                x.Strand == "+",
+                ((x.Start + x.End) // 2 - x.Start_ref) / x.len_of_feature
+                + x.frac_of_feature,
+                (x.End_ref - (x.Start + x.End) // 2) / x.len_of_feature
+                + x.frac_of_feature,
+            )
         )
     )
     df["d_norm"] = np.where(
